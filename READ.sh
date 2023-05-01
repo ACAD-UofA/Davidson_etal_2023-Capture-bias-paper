d943839f2281b2e220199b7ea1c6a35fe4575af0
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2023-05-01 15:52:27 +0930

    Create READ.sh

---
+++ READ.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DATA=$1
+
+ml plink
+
+#Prepare dataset
+plink --bfile ${DATA} --keep-allele-order --maf 0.01 --geno 0.999999 --mind 1.0 --allow-no-sex --recode transpose --out ${DATA}
+
+rm ${DATA}.nosex
+
+awk '{print $1,$2"__",$3,$4,$5,$6}' ${DATA}.tfam > ${DATA}.2.tfam
+
+mv ${DATA}.2.tfam ${DATA}.tfam
+
+# Run READ
+python2 $PATH/READ.py ${DATA}
+
+# Count SNPs per pair
+pairs = $(awk '{print $1}' READ_output_ordered | sort | uniq)
+
+for i in pairs; do
+	grep "${i}" READ_output_ordered > ${i}_READ_output_ordered
+done
+
+awk '{s+=$4} END {print s}' *_READ_output_ordered > READ_snps_per_pair  
