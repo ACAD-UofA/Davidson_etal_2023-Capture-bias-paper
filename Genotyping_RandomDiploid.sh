5e89b1b8ff152e3dc3fa1a7054e8e29ae43f4477
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2023-05-01 16:06:29 +0930

    Create Genotyping_RandomDiploid.sh

---
+++ Genotyping_RandomDiploid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+conda activate sequencetools
+ml SAMtools/1.8
+
+map=$PATH
+
+## Prime Plus Capture 1240K plus Y46K
+samtools mpileup -R -B -q20 -Q30 --positions ${map}/PrimePlus_CaptureBED/PrimePlus_1240k_Y46k_sorted.pos \
+        --fasta-ref ${map}/GRCh37/human_g1k_v37_decoy.fasta \
+        --bam-list bamlist.txt | pileupCaller --sampleNameFile newSamplelist.txt \
+	--snpFile ${map}/PrimePlus_CaptureBED/PrimePlus_1240k_Y46k_sorted.snp \
+	--randomDiploid \
+	--samplePopName UN_MAJ \
+	--eigenstratOut AP_diploid
