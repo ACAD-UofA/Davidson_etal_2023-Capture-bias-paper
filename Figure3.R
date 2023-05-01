3ff313cc305bea3bd5485c3b638da69bd2cfdfbc
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2023-05-01 15:17:36 +0930

    Update Figure3.R

--- Figure3.R
+++ Figure3.R
@@ -6,15 +6,13 @@ library(ggtext)
 library(patchwork)
 library(seecolor)
 
-setwd("/Users/robertadavidson/Box Sync/Robbi_PhD/09_Arbor_Bias_Chapter") #set working directory
-
-anno = readxl::read_excel("/Users/robertadavidson/Box Sync/Robbi_PhD/LABELS.xls", sheet = "v54.1_1240K_public", col_names = TRUE)
-lbl = readxl::read_excel("/Users/robertadavidson/Box Sync/Robbi_PhD/LABELS.xls", sheet = "SA_PCA_labels", col_names = TRUE)
+anno = readxl::read_excel("./LABELS.xls", sheet = "v54.1_1240K_public", col_names = TRUE)
+lbl = readxl::read_excel("./LABELS.xls", sheet = "SA_PCA_labels", col_names = TRUE)
 
 #prep EMU data
-fn = "/Users/robertadavidson/Box Sync/Robbi_PhD/09_Arbor_Bias_Chapter/worldPCA_1.eigenvecs"
+fn = "./worldPCA_1.eigenvecs"
 emucDat1 = read.table(fn, col.names=c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10")) #emu output
-ind = "/Users/robertadavidson/Box Sync/Robbi_PhD/09_Arbor_Bias_Chapter/worldPCA_1.ind"
+ind = "./worldPCA_1.ind"
 indDat1 = read.table(ind, col.names=c("Sample","Sex","Pop"))
 emuDat1 = cbind(emucDat1, indDat1)
 
@@ -31,10 +29,8 @@ anc <- select(ancientemu, Sample, AP_label, Type) %>%
   mutate(f3group = paste(AP_label,Type, sep="_")) 
 head(anc)
 
-write_tsv(anc, "/Users/robertadavidson/Box Sync/Robbi_PhD/09_Arbor_Bias_Chapter/AP_ancient_all_1.ID.txt")
-
 #prep smartPCA data
-fn = "/Users/robertadavidson/Box Sync/Robbi_PhD/09_Arbor_Bias_Chapter/worldPCA_1.outlierremoval.pca.evec.txt"
+fn = "./worldPCA_1.outlierremoval.pca.evec.txt"
 evecDat1 = read.table(fn, col.names=c("Sample","PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10","group")) #smartpca output
 evecDat1 <- left_join(evecDat1,lbl, by=c("Sample"="ID"))
 evecDat1 <- left_join(evecDat1,anno, by=c("Sample"="Genetic_ID"))
@@ -56,23 +52,17 @@ labels <- c("Pampas","South Argentina","Bolivia","Brazil","Central Chile","South
 
 colours34 <- c("#AC37B2","#865CD8","#F2C74B","#669F1A","#407DB1","#2238B6","#D96443","#F09035","#BF7932","firebrick4","darkslategray4",
                "grey50","grey80","black")
-seecolor::print_color(colours34)
-
-#count number of each data type
-table(evecDat1$Type)
 
 ## plot
-PC1_2_regionemu <- ggplot() + #each species is represented by a different shape
+PC1_2_regionemu <- ggplot() + 
   geom_point(data=modernemu, aes(PC1,PC2), colour="grey") +
-  geom_point(data=ancientemu, aes(PC1,PC2, fill = Populations), shape = 21, size = 4, alpha=1, stroke = 0.5) + #customised scatter plot 
+  geom_point(data=ancientemu, aes(PC1,PC2, fill = Populations), shape = 21, size = 4, alpha=1, stroke = 0.5) + 
   guides(fill=guide_legend(ncol =1)) + guides(colour=guide_legend(ncol =1)) + guides(shape=guide_legend(ncol =1)) + 
   scale_fill_manual(name="Population",labels=labels, values = colours34) +
- # geom_text_repel(data = modern, aes(label=__Political_Entity, y = PC1, x =PC2), segment.size  = 0.2, 
- #                 segment.color = "grey80", colour="black", force=4, show.legend = FALSE, max.overlaps = Inf, size=2) +
   geom_text(aes(label="Africa", x=-0.04, y=0.08), family = "Helvetica") +
   geom_text(aes(label="Europe", x=-0.025, y=-0.02), family = "Helvetica") +
   geom_text(aes(label="Americas", x=0.03, y=0.02), family = "Helvetica") +
-  theme_bw() + #light theme with no grey background and with grey lines and axes
+  theme_bw() + 
   geom_vline(xintercept=0, linetype = 3, colour="grey10") +
   geom_hline(yintercept=0, linetype = 3, colour="grey10") +
   scale_x_continuous(sec.axis = dup_axis()) + #add duplicated secondary axis on top 
@@ -80,48 +70,41 @@ PC1_2_regionemu <- ggplot() + #each species is represented by a different shape
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(), #remove minor gridline
         axis.title.x.top = element_blank(), axis.text.x.top = element_blank(), #remove labels on top x-axis
         axis.title.y.right = element_blank(), axis.text.y.right = element_blank(), legend.text = element_markdown(),
-        axis.ticks = element_blank())  #remove labels on right y-axis
+        axis.ticks = element_blank()) 
 PC1_2_regionemu #show plot
-#ggplotly(PC1_2_region)
 
 #ggplot 1v2
-PC1_2_typeemu <- ggplot() + #each species is represented by a different shape
+PC1_2_typeemu <- ggplot() + 
   geom_point(data=modernemu, aes(PC1,PC2), colour="grey") +
-  geom_point(data=ancientemu, aes(PC1,PC2, fill = Type,colour=Type, shape = Type), size = 4, alpha=1, stroke = 0.3) + #customised scatter plot 
+  geom_point(data=ancientemu, aes(PC1,PC2, fill = Type,colour=Type, shape = Type), size = 4, alpha=1, stroke = 0.3) + 
   guides(fill=guide_legend(ncol =1)) + guides(colour=guide_legend(ncol =1)) + guides(shape=guide_legend(ncol =1)) + 
   scale_shape_manual(values = c(1,4,5)) +
-  #scale_shape_manual(values = c(1,0,5)) +
   scale_fill_manual(values = c("tomato2","limegreen","grey20")) +
   scale_colour_manual(values = c("tomato2","limegreen","black")) +
-  #geom_text_repel(data = subset(evecDat1, AP_label=="Spain"), aes(label=Sample,y = PC1, x =PC2), segment.size  = 0.2, 
-  #segment.color = "grey80", colour="black", force=4, show.legend = FALSE, max.overlaps = Inf, size=2) +
   geom_text(aes(label="Africa", x=-0.04, y=0.08), family = "Helvetica") +
   geom_text(aes(label="Europe", x=-0.025, y=-0.02), family = "Helvetica") +
   geom_text(aes(label="Americas", x=0.03, y=0.02), family = "Helvetica") +
-  theme_bw() + #light theme with no grey background and with grey lines and axes
+  theme_bw() + 
   geom_vline(xintercept=0, linetype = 3, colour="grey10") +
   geom_hline(yintercept=0, linetype = 3, colour="grey10") +
-  scale_x_continuous(sec.axis = dup_axis()) + #add duplicated secondary axis on top 
-  scale_y_continuous(sec.axis = dup_axis()) + #add duplicated secondary axis on right
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(), #remove minor gridline
-        axis.title.x.top = element_blank(), axis.text.x.top = element_blank(), #remove labels on top x-axis
+  scale_x_continuous(sec.axis = dup_axis()) + 
+  scale_y_continuous(sec.axis = dup_axis()) + 
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(), 
+        axis.title.x.top = element_blank(), axis.text.x.top = element_blank(), 
         axis.title.y.right = element_blank(), axis.text.y.right = element_blank(), legend.text = element_markdown(),
-        axis.ticks = element_blank(), legend.position = c(0.8,0.8))  #remove labels on right y-axis
+        axis.ticks = element_blank(), legend.position = c(0.8,0.8))  
 PC1_2_typeemu #show plot
 
-#ggplotly(PC1_2_type)
 ## plot
-PC1_2_regionPCA <- ggplot() + #each species is represented by a different shape
+PC1_2_regionPCA <- ggplot() + 
   geom_point(data=modernPCA, aes(PC1,-PC2), colour="grey") +
-  geom_point(data=ancientPCA, aes(PC1,-PC2, fill = Populations), shape = 21, size = 4, alpha=1, stroke = 0.5) + #customised scatter plot 
+  geom_point(data=ancientPCA, aes(PC1,-PC2, fill = Populations), shape = 21, size = 4, alpha=1, stroke = 0.5) +  
   guides(fill=guide_legend(ncol =1)) + guides(colour=guide_legend(ncol =1)) + guides(shape=guide_legend(ncol =1)) + 
   scale_fill_manual(name="Population",labels=labels, values = colours34) +
-  # geom_text_repel(data = modern, aes(label=__Political_Entity, y = PC1, x =PC2), segment.size  = 0.2, 
-  #                 segment.color = "grey80", colour="black", force=4, show.legend = FALSE, max.overlaps = Inf, size=2) +
   geom_text(aes(label="Africa", x=-0.09, y=0.04), family = "Helvetica") +
   geom_text(aes(label="Europe", x=0.02, y=-0.05), family = "Helvetica") +
   geom_text(aes(label="Americas", x=0.01, y=0.05), family = "Helvetica") +
-  theme_bw() + #light theme with no grey background and with grey lines and axes
+  theme_bw() + 
   geom_vline(xintercept=0, linetype = 3, colour="grey10") +
   geom_hline(yintercept=0, linetype = 3, colour="grey10") +
   scale_x_continuous(sec.axis = dup_axis()) + #add duplicated secondary axis on top 
@@ -129,25 +112,21 @@ PC1_2_regionPCA <- ggplot() + #each species is represented by a different shape
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(), #remove minor gridline
         axis.title.x.top = element_blank(), axis.text.x.top = element_blank(), #remove labels on top x-axis
         axis.title.y.right = element_blank(), axis.text.y.right = element_blank(), legend.text = element_markdown(),
-        axis.ticks = element_blank())  #remove labels on right y-axis
+        axis.ticks = element_blank())  
 PC1_2_regionPCA #show plot
-#ggplotly(PC1_2_region)
 
 #ggplot 1v2
-PC1_2_typePCA <- ggplot() + #each species is represented by a different shape
+PC1_2_typePCA <- ggplot() + 
   geom_point(data=modernPCA, aes(PC1,-PC2), colour="grey") +
-  geom_point(data=ancientPCA, aes(PC1,-PC2, fill = Type, colour = Type, shape = Type), size = 4, alpha=1, stroke = 0.3) + #customised scatter plot 
+  geom_point(data=ancientPCA, aes(PC1,-PC2, fill = Type, colour = Type, shape = Type), size = 4, alpha=1, stroke = 0.3) + 
   guides(fill=guide_legend(ncol =1)) + guides(colour=guide_legend(ncol =1)) + guides(shape=guide_legend(ncol =1)) + 
   scale_shape_manual(values = c(1,4,5), labels=c("1240k","PP","Shotgun")) +
-  #scale_shape_manual(values = c(1,0,5)) +
   scale_fill_manual(values = c("tomato2","limegreen","grey20"), labels=c("1240k","PP","Shotgun")) +
   scale_colour_manual(values = c("tomato2","limegreen","black"), labels=c("1240k","PP","Shotgun")) +
-  #geom_text_repel(data = subset(evecDat1, AP_label=="Spain"), aes(label=Sample,y = PC1, x =PC2), segment.size  = 0.2, 
-  #segment.color = "grey80", colour="black", force=4, show.legend = FALSE, max.overlaps = Inf, size=2) +
   geom_text(aes(label="Africa", x=-0.09, y=0.04), family = "Helvetica") +
   geom_text(aes(label="Europe", x=0.02, y=-0.05), family = "Helvetica") +
   geom_text(aes(label="Americas", x=0.01, y=0.05), family = "Helvetica") +
-  theme_bw() + #light theme with no grey background and with grey lines and axes
+  theme_bw() + 
   geom_vline(xintercept=0, linetype = 3, colour="grey10") +
   geom_hline(yintercept=0, linetype = 3, colour="grey10") +
   scale_x_continuous(sec.axis = dup_axis()) + #add duplicated secondary axis on top 
@@ -155,7 +134,7 @@ PC1_2_typePCA <- ggplot() + #each species is represented by a different shape
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(), #remove minor gridline
         axis.title.x.top = element_blank(), axis.text.x.top = element_blank(), #remove labels on top x-axis
         axis.title.y.right = element_blank(), axis.text.y.right = element_blank(), legend.text = element_markdown(),
-        axis.ticks = element_blank())  #remove labels on right y-axis
+        axis.ticks = element_blank())  
 PC1_2_typePCA #show plot
 
 #panel
