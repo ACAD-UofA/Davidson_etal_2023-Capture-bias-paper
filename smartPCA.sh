2ae84a3e4f2ff11a7e7038ffdae471160c980b93
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2023-08-23 12:49:05 +0930

    Update smartPCA.sh
    update to eigensoft 8.0.0

--- smartPCA.sh
+++ smartPCA.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
+module load Singularity/3.10.5
+
 in1=$1
-in2=poplist.txt
+in2=$2
 
-## NB: Use "outliermode" when calculating PCA from missingness. 
-## Use "poplistname" when projecting
-## EIGENSOFT = 
-## smartPCA version 
+#Use poplistname to define populations used in eigenvector calculation
+#Use missingmode to calculate PCA from SNP missingness rather than genotypr
 
-smartpca -p <(echo "genotypename:	${in1}.geno
+singularity exec <path>/eigensoft_8.0.0--h6a739c9_3.sif smartpca -p <(echo "genotypename:	${in1}.geno
 snpname:	${in1}.snp
 indivname:	${in1}.ind
-evecoutname:	${in1}.pca.evec.txt
-evaloutname:	${in1}.pca.eval.txt
+evecoutname:	${in1}.8.0.pca.evec.txt
+evaloutname:	${in1}.8.0.pca.eval.txt
 poplistname:	${in2}
-#missingmode:	YES
+missingmode:	NO
 lsqproject:	YES
-outliermode:	2
-outlieroutname:	${in1}.pca.outlier.txt
-snpweightoutname:	${in1}.pca.snpweight.txt
+outliermode:	1
+outlieroutname:	${in1}.8.0.pca.outlier.txt
+snpweightoutname:	${in1}.8.0.pca.snpweight.txt
 numthreads:	4
 newshrink:	YES
 maxpops:	500
